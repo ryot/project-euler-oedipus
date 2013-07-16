130122e822047480b2aad46fbfa91cd226aac605
Ryo Tulman <ryo.tulman@gmail.com>
2013-07-15 20:18:39 -0700

    finished problem 5

--- project-euler-oedipus.py
+++ project-euler-oedipus.py
@@ -5,8 +5,11 @@
 # Thank you Tuhin Srivasta @Gumroad for introducing me to Project Euler
 
 import sys
+import math
+from datetime import datetime
 
 def oedipus(p):
+	start = datetime.now()
 	myAnswer = 0
 
 	if (p == 1):
@@ -24,17 +27,51 @@ def oedipus(p):
 			prev = curr
 			curr = new
 
-	# elif (p == 3):
-	# 	#find largest prime factor of num
-	# 	num = 600851475143
-	# 	myAnswer = 2
-	# 	while num%myAnswer != 0:
-	# 		myAnswer += 1
+	elif (p == 3):
+		num = 600851475143
+		currDivisor = 2
+		while num > 1:
+			if num%currDivisor == 0:
+				num /= currDivisor
+				currDivisor = 2
+			else:
+				currDivisor += 1
+				myAnswer = currDivisor
 	
+	elif (p == 4):
+		for a in range(100, 1000):
+			for b in range(100, 1000):
+				p = a * b
+				prod = str(p)
+				prodReversed = prod[::-1]
+				if prodReversed == prod:
+					if int(prod) > myAnswer:
+						myAnswer = int(prod)
+
+	elif (p == 5):
+		myAnswer = math.factorial(20)
+		divisors = range(1, 21)
+		answerFound = False
+		while answerFound is False:
+			startAnswer = myAnswer
+			for n in divisors:
+				testQuotient = myAnswer/n
+				works = True
+				for p in divisors:
+					if testQuotient%p != 0:
+						works = False
+						break
+				if works is True:
+					myAnswer = testQuotient
+			if myAnswer == startAnswer:
+				answerFound = True
+
 	else:
 		myAnswer = "I'm stumped. Goodbye world."
 
+	end = datetime.now()
 	print myAnswer
+	print end - start
 	print "THE END"
 
 
